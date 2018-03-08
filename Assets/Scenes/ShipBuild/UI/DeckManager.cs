16306fcf7886011ffc665bc26cf5190801af2fc0
Jack.Schaible <jack.schaible@ama.ab.ca>
2018-03-08 16:21:08 -0700

    Modal Overlay and Deck Manager
    Added an overlay to the modal
    Added deck buttons, currently non-functional
    
    TODO: Need to add top and bottom new deck buttons attach to script

---
+++ Assets/Scenes/ShipBuild/UI/DeckManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+namespace Assets.Scenes.ShipBuild
+{
+    public class DeckManager : MonoBehaviour
+    {
+        public Button NewUpperDeck;
+        public Button NewLowerDeck;
+
+        private List<Button> _deckButtons;
+
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
